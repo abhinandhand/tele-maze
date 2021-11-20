e2ab39f9bbf41a52cfef17d7bbcd58817601241e
Abhinandhan Devadiga <Abhinandhan.Devadiga@snappet.org>
2021-11-20 01:08:33 +0100

    handling path params & query param in url

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SearchRoutingModule } from './search-routing.module';
-import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { tvShowReducer, tvShowsStateFeatureKey } from 'src/app/store/tv-shows/reducers';
 import { SearchEffects } from './effects/search.effects';
+import { ResultsComponent } from './components/results/results.component';
+import { SearchRoutingModule } from './search-routing.module';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ResultsComponent
+  ],
   imports: [
     CommonModule,
     SearchRoutingModule,
